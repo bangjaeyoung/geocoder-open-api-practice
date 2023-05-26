37c93d1d314a9e5a08f84dee1b93ae1d93896d9b
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-26 13:09:18 +0900

    first commit

---
+++ src/main/java/com/example/geocoder/GeocoderApplication.java
@@ -0,0 +1,10 @@
+package com.example.geocoder;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderApplication {
+
+    public static void main(String[] args) {
+    }
+}
