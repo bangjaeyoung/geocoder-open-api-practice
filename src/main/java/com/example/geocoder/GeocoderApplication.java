429bdce0a11b4702ce714b59f402738c0c608ee3
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-26 16:20:56 +0900

    webclient 라이브러리 추가 및 설정 변경

--- src/main/java/com/example/geocoder/GeocoderApplication.java
+++ src/main/java/com/example/geocoder/GeocoderApplication.java
@@ -1,10 +1,12 @@
 package com.example.geocoder;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeocoderApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(GeocoderApplication.class, args);
     }
 }
