3a2d81aa73506aa919c74470c70661d2c26a3e33
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-26 16:26:34 +0900

    의존성 주입방식 변경

--- src/main/java/com/example/geocoder/controller/GeoPointController.java
+++ src/main/java/com/example/geocoder/controller/GeoPointController.java
@@ -2,22 +2,17 @@ package com.example.geocoder.controller;
 
 import com.example.geocoder.service.GeoPointService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class GeoPointController {
 
     private final GeoPointService geoPointService;
-
-    @Autowired
-    public GeoPointController(GeoPointService geoPointService) {
-        this.geoPointService = geoPointService;
-    }
 
     @GetMapping
     public Mono<ResponseEntity<String>> getGeoPoint() {
